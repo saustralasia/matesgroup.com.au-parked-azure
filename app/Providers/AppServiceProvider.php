6e95e224d1810b6c51f992f1df29ba47e3d2b339
Engr. Junayed Chowdhury <bdjunayed@live.com>
2020-03-04 10:35:07 +0600

    Add following code to AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        Builder::defaultStringLength(191); // Update defaultStringLength
     }
 }
