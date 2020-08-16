27a3dcb7229b4baa9172df92540c1b59d30f6864
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-08-16 16:23:27 +0600

    -content writing of construction-staff-recruitment

--- resources/views/construction-staff-recruitment.blade.php
+++ resources/views/construction-staff-recruitment.blade.php
@@ -14,7 +14,23 @@
 
         <div class="container">
             <div class="col-md-12" class="text-justify">
-                <p>Under construction!</p>
+                <p><strong>Mates Group</strong> is an Australian company with clients around the world. Proudly representing major agribusiness, large primary producers and rural operators. We provide pickers, packer, turf cleaners and more suitable and experienced workers.</p>
+                <h2><strong>Careers at Mates Group </strong></h2>
+                <p><strong>Mates Group</strong> office support provides specialised office support staff Australia wide. The main contribution of our company is to have the right kind of staff who help to achieve goals in business and reach the required targets. We are the <strong>best</strong> construction staff recruiter in Australia. Side by side we also recruit skillful construction labourer. That's why we are the best construction staff recruitment service & labourer recruitment service in Australia.</p>
+                <p><strong>Why choose us?<br /> </strong>We are an elite team of recruitment experts, encouraging professionalism, ethical conduct, and best practices.<br>If you are seeking qualified, part-qualified or technically experienced construction staff, we’ll give you access to more talent, more quickly and in more locations than any other specialist recruitment company.</p>
+
+
+                <h2><strong>It's time to construct</strong></h2>
+                <p><strong>We specialise in recruiting for the construction, property, engineering, design, & resources sectors, and with recruitment specialists in each industry, we ensure that top talent is matched with top companies.</strong></p>
+                <p>We are dedicated to providing the highest levels of service, offering a personalised service to both companies and individuals. We have a strong industry network, and our knowledge base allows us to partner companies with the individuals they need. Focusing on innovation and results, we manage the recruitment process in a way that allows us to achieve consistently great results.</p>
+
+                <p>We aim to give the most specialised and personalised recruitment experience to our clients and candidates, to ensure that both parties recieve top quality service and have their needs met.</p>
+
+                <p>We connect candidates with leading Australian companies across the construction, architecture, design, engineering, mining and resource sectors. We have a team of experienced reccruiters who specialise in each of these sectors to assist you in furthering your career, or building your team.</p>
+
+                <p>Our recruitment specialists are among the leading recruiters in Australia, and provide the highest quality service, matching top talent with top companies.</p>
+
+
             </div>
         </div>
     </div>
