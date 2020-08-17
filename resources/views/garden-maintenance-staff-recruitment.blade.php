4b74a86a48a78547b9c33a4a06e809d2d6329359
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-08-17 15:13:23 +0600

    -content writing of garden-maintenance-staff-recruitment

--- resources/views/garden-maintenance-staff-recruitment.blade.php
+++ resources/views/garden-maintenance-staff-recruitment.blade.php
@@ -14,7 +14,27 @@
 
         <div class="container">
             <div class="col-md-12" class="text-justify">
-                <p>Under construction!</p>
+                <p style="font-size:30px; color:#660066;"><strong>We provide warehouse staff recruitment service Australia</strong></p>
+                <p style="font-size:20px; color: #3366ff"><strong>Looking for gardener for your garden?</strong></p>
+                <p><strong>Mates Group</strong> provides you skilled gardener. We will let you to have the skilled & qualified gardener. We are the perfect garden maintenance staff hire company Australia</p>
+                <p>As one of the leading recruitment agencies in Australia, we know that the recruitment process is very tough, so we want to remove your stress. We will handle the hiring process for you. Our target is to assure that you can get the right person for your company. </p>
+                <p style="font-size:20px; color: #3366ff"><strong>Why us?</strong></p>
+                <p>We always supply our work force ensuring that they are the best fit in your company. If they are dedicated & strong enough, only then we supply them to you. So, this must save your time and money both for the best recruitment in your company. We have a big database of workers so that we are the perfect worker provider. So, if you are searching for skilled gardener, then without any doubt you can contact us.</p>
+                <p>We provide the following skilled person as a gardener:<br>
+                    <ul style="padding-left: 40px">
+                        <li>-interested in gardening and horticulture</li>
+                        <li>-working in a team</li>
+                        <li>-communication skills</li>
+                        <li>-physically fit for digging, lifting, carrying, etc</li>
+                        <li>-willing to work outside in all weathers</li>
+                        <li>-able to use tools, including power tools</li>
+                        <li>-able to work flexibly, according to seasons and weather</li>
+                        <li>-practical skills</li>
+                        <li>-have an eye for design and colour</li>
+
+                    </ul>
+                </p>
+
             </div>
         </div>
     </div>
