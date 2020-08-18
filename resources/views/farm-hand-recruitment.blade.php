f8cda57ed0a88dd78d54891b461f7525a66d6e91
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-08-18 19:35:06 +0600

    -content writing of farm-hand-recruitment

--- resources/views/farm-hand-recruitment.blade.php
+++ resources/views/farm-hand-recruitment.blade.php
@@ -5,8 +5,39 @@
 @section('meta_des', 'Are you looking for farm hand staff hiring solutions? Mates Group is a leading company that offers recruitment services in Australia & worldwide.')
 
 @section('content')
-    <center>
-        <h1 class="extra-h2">Leading Process Staff & Labour Hire Company</h1>
-        <p>UNDER CONSTRUCTION</p>
-    </center>
+
+    <div class="header-banner">
+        <center>
+            <h1 class="white line-12 text-45">Leading Process Staff & Labour Hire Company</h1>
+        </center>
+    </div>
+    <div class="single-content">
+        <div class="container">
+            <center>
+                <p>Farming is a part of agriculture. It started thousands years ago. Eventually, people used domesticated animals such as oxen for ploughing, pulling, and transportation. Agriculture enabled people to produce surplus food. They could use this extra food when crops failed or trade it for other goods. Food surpluses allowed people to work at other tasks unrelated to farming.<br>
+                    If you are looking for farm work in Australia, then please contact with us soon! We offer farm hand staff recruitment service in Australia.<br>
+                    Candidates with agricultural experience in tractor, header/combine or truck driving will get a plus point.
+                </p>
+            </center>
+
+            <div style="margin-top: 70px;">
+                <p style="font-size: 30px; text-decoration: underline;">Connect The Candidates</p>
+                <p>Here you can find your dreamed farm jobs that we recruit for in New South Wales, Victoria, Western Australia, South Australia, the Australian Capital Territory Tasmania, and the Northern Territory. <br>We recruit large number of farm job employees in Australia. We connect qualified candidates then we find out the farm jobs in Australia to make our candidates employed.</p>
+
+                <p style="font-size: 30px; text-decoration: underline;">Horticulture roles</p>
+                <p>We always placed our candidates in skilled and unskilled roles. Fruit picking is a highly demandable job. In different times of the year our clients want fruit picking employees.<br>
+                    We connect our clients and candidates for fruit picking and packing roles. We also provide candidates for Quality Assurance position.<br>
+                    We consult with our clients and offer training programs for our candidates to make themselves perfect in this sector. We provide the trusted farm hand staff recruitment Australia.
+                </p>
+
+                <p style="font-size: 30px; text-decoration: underline;">Livestock roles</p>
+                <p>Livestock is commonly defined as domesticated animals raised in an agricultural setting to produce labor and commodities such as meat, eggs, milk, fur, leather, and wool.<br>
+                    We offer our candidates to our livestock clients who run poultry, dairy farming, and aquaculture operations. Fortunately, we have a number of clients as well as reliable workers in this sector</p>
+
+                <p style="font-size: 30px; text-decoration: underline;">Other farm jobs in Australia</p>
+                <p>We also assist our clients in other farming sectors. We are proud to help our clients and our employees. </p>
+            </div>
+
+        </div>
+    </div>
 @endsection
