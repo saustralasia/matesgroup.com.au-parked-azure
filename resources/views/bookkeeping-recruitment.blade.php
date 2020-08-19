c14b33229377ff9542b26751c311f6079b5c658d
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-08-19 15:28:08 +0600

    -content writing of bookkeeping-recruitment

--- resources/views/bookkeeping-recruitment.blade.php
+++ resources/views/bookkeeping-recruitment.blade.php
@@ -5,8 +5,31 @@
 @section('meta_des', 'Mates Group is a reputed company with clients around the world. We provide bookkeeper staff recruitment or hire solutions in Australia & worldwide.')
 
 @section('content')
-    <center>
-        <h1 class="extra-h2">Mates Group: Trusted Bookkeeper Hire Company</h1>
-        <p>UNDER CONSTRUCTION</p>
-    </center>
+    <div class="header-banner">
+        <center>
+            <h1 class="white line-12 text-45">Mates Group: Trusted Bookkeeper Hire Company</h1>
+        </center>
+    </div>
+    <div class="single-content">
+        <div class="container">
+            <p style="font-size: 30px; text-align: center;"><strong>Book keeper recruitment service Australia</strong></p>
+            <p>Bookkeepers are liable for keeping up the money related records of a business or organization, checking exchanges, refreshing explanations and guaranteeing precision notwithstanding delivering budget summaries and different reports for the board and administrators. Now-a-days bookkeeper hire service worldwide is one of the most necessary thing in all over the world. So, we are here to help you with our bookkeeper hire service Australia.</p>
+            <p>We will guarantee exact budgetary reports so you can generally settle on better choices for your business. </p>
+            <p>We can help you in the following issues:</p>
+
+                <ul style="padding-left: 40px;">
+                    <li>•   We help accomplish entrepreneur smooth out and rearrange all your accounting and bookkeeping forms. </li>
+                    <li>•	We fill in as your inner bookkeeper to get every one of your frameworks set up. </li>
+                    <li>•	Daily, week by week or month to month handling of your records in your framework. </li>
+                    <li>•	Reconciliation of indebted individuals (records of sales) – deals solicitations, proclamations, receipts. </li>
+                    <li>•	Reconciliation of loan bosses (creditor liabilities) – section of solicitations, proclamations, inquiries and lender instalments. </li>
+                    <li>•	Budgeting and anticipating – help with the readiness, announcing and audit of spending plans. </li>
+                    <li>•	Credit Control is a basic movement for some entrepreneurs – we will effectively seek after remarkable solicitations owed by your clients. </li>
+                    <li>•	Life line of the entrepreneur is money so we will screen your income following and report on a week by week premise. </li>
+                    <li>•	Book a call to find what you can accomplish.</li>
+                </ul>
+
+            <p></p>
+        </div>
+    </div>
 @endsection
