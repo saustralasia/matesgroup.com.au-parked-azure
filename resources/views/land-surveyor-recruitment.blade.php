f2234f7fe677d26b63af810f17d44ee90e55eb00
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-08-25 16:30:52 +0600

    new meta_keywords adding in -land-surveyor-recruitment

--- resources/views/land-surveyor-recruitment.blade.php
+++ resources/views/land-surveyor-recruitment.blade.php
@@ -1,7 +1,51 @@
 @extends('layouts.master', ['title' => 'Our Vision'])
 
 @section('title', 'Best Land Surveyor Recruitment/Hire Service Australia')
-@section('meta_keywords', 'Land surveyor recruitment australia, land surveyor recruitment service, land surveyor hire specialist worldwide')
+@section('meta_keywords', 'Land surveyor recruitment australia, land surveyor recruitment service,
+land surveyor hire specialist worldwide,
+land surveyor recruitment in Sydney,
+land surveyor recruitment in Melbourne,
+land surveyor recruitment in Queensland,
+land surveyor recruitment in Victoria,
+land surveyor recruitment in NSW,
+land surveyor recruitment in Brisbane,
+
+
+land surveyor hire services in Sydney,
+land surveyor hire services in Melbourne,
+land surveyor hire services in Queensland,
+land surveyor hire services in Victoria,
+land surveyor hire services in NSW,
+land surveyor hire services in Brisbane,
+
+
+land surveyor recruitment services in Sydney,
+land surveyor recruitment services in Queensland,
+land surveyor recruitment services in Melbourne,
+land surveyor recruitment services in NSW,
+land surveyor recruitment services in Victoria,
+land surveyor recruitment services in Brisbane,
+
+
+land surveyor regional job Queensland,
+land surveyor regional job Sydney,
+land surveyor regional job Melbourne,
+land surveyor regional job NSW,
+land surveyor regional job Victoria,
+land surveyor regional job Brisbane,
+
+
+local land surveyor recruitment company,
+local land surveyor hire company,
+
+
+local land surveyor recruitment company in Sydney,
+local land surveyor recruitment company in Melbourne,
+local land surveyor recruitment company in Queensland,
+local land surveyor recruitment company in Victoria,
+local land surveyor recruitment company in NSW,
+local land surveyor recruitment company in Brisbane,
+')
 @section('meta_des', 'Mates Group is a reputed company with clients around the world. We provide land surveyor recruitment or hire solutions in Australia & worldwide.')
 
 @section('content')
