ca0a2e6c06a180b1d019f1e3abcae9aff9cedf7b
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-10-07 11:14:32 +0600

    -Agriculture Recruitment -Casual labour hire -Engineering Recruitment -International Recruitment -IT Recruitment -Payroll Recruitment -Recruitment agency,labour hire company -Shipbuilding Recruitment -Transport Recruitment -Warehouse Recruitment -Construction Recruitment -Office Support Recruitment
    these pages are ready to deploy

---
+++ resources/views/maintenance-recruitment.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.master')
+
+@section('title', 'Maintenance Engineering & Manager Recruitment Agency Australia')
+@section('meta_keywords', 'maintenance recruitment agency australia, maintenance engineering recruitment agency australia, maintenance manager recruitment agency australia, maintenance engineering hire agency australia, maintenance engineering recruitment services australia')
+@section('meta_des', 'Australian company Mates Group provides maintenance engineer, maintenance manager hire services in Melbourne, Sydney, Adelaide and Brisbane.')
+
+@section('content')
+    <div class="header-banner">
+        <h1 class="white line-12 text-45" style="text-align: center;">Maintenance Recruitment</h1>
+    </div>
+    <div class="single-content">
+
+        <div class="single-content">
+            <div class="container">
+                <p>
+                    Maintenance managers and engineers are like the
+                    DNA of a project. Without DNA a cell is considered dead.
+                    Like that, without a maintenance manager and engineers, a project cannot be successful.
+                </p>
+
+                <p>
+                    In Australia, there are so many business type
+                    companies that depend on maintenance experts.
+                    They are seeking for experienced and also fresher
+                    maintenance specialists. We, Mates Group, are successfully
+                    working as a maintenance recruitment agency in Australia for many years.
+                </p>
+
+                <p>
+                    There are so many recruitment agencies in Australia
+                    but we always try to be different from them with
+                    our scientific modern process and our skilled
+                    masterminded recruitment team. To recruit the
+                    best-fit candidates is a tough process. We do not
+                    want you struggling with this. We are here ready to help you.
+                    Let our experienced recruiting team handle this hassle job
+                    for you. We grab skilled and dedicated workers for your company.
+                    As a maintenance hiring expert in Australia, we have the experience
+                    that so many companies do struggle and spend their valuable
+                    time recruiting candidates but at last, it goes into vain.
+                    Just wasting time and money. Please, do not do that.
+                    Just place this difficult job into our hands.
+                    We care about the value of your time and money.
+                </p>
+
+                <p>
+                    Every year we provide mechanical engineers,
+                    electrical engineers, programmers, technicians,
+                    architects to different companies in Australia.
+                    Our candidates also do their best to uphold our companies’
+                    name and fame. We provide our candidates in Melbourne,
+                    Sydney, Adelaide, Brisbane as well as all over in Australia.
+                    Our supporting team is always ready to handle any of
+                    your problems occurred when working.
+                </p>
+
+                <p style="font-size: 20px;"><strong>Senior Engineer – Maintenance</strong></p>
+                <p>
+                    Mates Group can arrange Senior Maintenance Engineer
+                    with the following skill which has been described
+                    below in the full job description. We can provide one to
+                    ten years of industry experienced maintenance engineers.
+                    We can arrange staff for Melbourne, Sydney, Adelaide and Brisbane.
+                </p>
+
+                <p style="font-size: 20px;"><strong>Job Description:</strong></p>
+                <p>
+                    Reporting to our maintenance recruitment
+                    manager the successful applicants will be a hands-on,
+                    energetic team player who wants the learning curve,
+                    the experience, and opportunities that this position will offer.
+                </p>
+                <p style="font-size: 20px;"><strong>Applicant should have the following qualities:</strong></p>
+
+                <div style="margin-left: 50px;margin-bottom: 20px;">
+                    <li>Bachelor’s Degree in the related fields.</li>
+                    <li>Proficiency in AutoCAD (if needed).</li>
+                    <li>Proficiency in Microsoft office essentials.</li>
+                    <li>Mechanical trade certificate.</li>
+                    <li>Any kind of technical fault and failure finding.</li>
+                    <li>Dedicated and motivated person in your job field.</li>
+                    <li>Previous experience will be a plus point.</li>
+                </div>
+
+                <p style="font-size: 20px;"><strong>Our commitment:</strong></p>
+                <p>
+                    Your success is our success and your failure
+                    is also ours’. So, if you are a company owner we
+                    will provide any kind of supports you need when working
+                    with our selected candidates. Side by side, if you are a candidate,
+                    we are also responsible to offer you the perfect job.
+                    So, if you are a new job seeker, you can start your career challenge with us.
+                    We are ready to hear that from you!
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
