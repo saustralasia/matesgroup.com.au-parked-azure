d3587d73d1f4b75d234f36b1317eb285a571c983
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-11-14 15:30:28 +0600

    recruitment.blade.php updated (recruitment aside)

--- resources/views/layouts/asides/recruitment.blade.php
+++ resources/views/layouts/asides/recruitment.blade.php
@@ -1,11 +1,13 @@
 <div class=" blog-aside-body">
     <h4 class="blog-aside-title mb-2">Casual labour hire</h4>
     <ol class="list-unstyled">
-        <li><a href="/assistant-accountant-recruitment">Casual labour hire</a></li>
-
-
-
-
+        <li><a href="/casual-labour-hire">Casual labour hire</a></li>
+        <li><a href="/international-recruitment">International Recruitment</a></li>
+        <li><a href="/recruitment-darwin">Recruitment Darwin</a></li>
+        <li><a href="/recruitment-gold-coast">Recruitment Gold Coast</a></li>
+        <li><a href="/recruitment-paramatta">Recruitment Paramatta</a></li>
+        <li><a href="/recruitment-sunshine">Recruitment Sunshine</a></li>
+        <li><a href="/temporary-recruitment">Temporary Recruitment</a></li>
 
 
     </ol>
