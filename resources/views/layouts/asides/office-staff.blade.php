3b56992409066669776449770be83e5ca064a407
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-11-14 15:35:48 +0600

    office-staff.blade.php updated (office-staff.blade.php aside)

--- resources/views/layouts/asides/office-staff.blade.php
+++ resources/views/layouts/asides/office-staff.blade.php
@@ -1,9 +1,14 @@
 <div class=" blog-aside-body">
     <h4 class="blog-aside-title mb-2">Office staff</h4>
     <ol class="list-unstyled">
-        <li><a href="/assistant-accountant-recruitment">Australia’s Assistant Accounting Hiring Specialist</a></li>
-
-
+        <li><a href="/assistant-accountant-recruitment">Assistant accountant recruitment</a></li>
+        <li><a href="/executive-assistant-recruitment">Executive assistant recruitment</a></li>
+        <li><a href="/financial-analyst-recruitment">Financial analyst recruitment</a></li>
+        <li><a href="/payroll-recruitment">Payroll recruitment</a></li>
+        <li><a href="/office-support-recruitment">Office support recruitment</a></li>
+        <li><a href="/executive-recruitment">Executive recruitment</a></li>
+        <li><a href="/receptionist-recruitment">Receptionist recruitment</a></li>
+        <li><a href="/it-recruitment">IT recruitment</a></li>
 
 
 
